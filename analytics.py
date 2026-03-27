78ffe1f6a24db157099b65879efd877155c3a61d
tasneemelsharkawy <t.elsharkawy2027@gmail.com>
2026-03-27 16:46:30 +0200

    Update analytics
    Added insights for total revenue, payment method, and top category, and saved them to text files.

--- analytics.py
+++ analytics.py
@@ -1,19 +1,35 @@
+import sys
 import pandas as pd
+import subprocess
 
-# 1. load cleaned data
-df = pd.read_csv("data_preprocessed.csv")
+if len(sys.argv) < 2:
+    sys.exit(1)
 
-# 2. total revenue
-total_revenue = df["Total Spent"].sum()
-print("Total Revenue:", total_revenue)
+df = pd.read_csv(sys.argv[1])
+orig_df = pd.read_csv("data_raw.csv")
 
-# 3. most used payment method
-top_payment = df["Payment Method"].value_counts().idxmax()
-print("Most used payment method:", top_payment)
+# Insight 1: total revenue
+total_revenue = orig_df["Total Spent"].sum()
+insight1 = f"Total Revenue: ${total_revenue:,.2f}"
 
-# 4. top selling category
-top_category = df["Category"].value_counts().idxmax()
-print("Top selling category:", top_category)
-# 5. top selling (Online/Instore)
-top_location = df["Location"].value_counts().idxmax()
-print("Top selling (Online/Instore):", top_location)
+# Insight 2: most used payment method
+top_payment = orig_df["Payment Method"].value_counts().idxmax()
+insight2 = f"Most used payment method: {top_payment}"
+
+# Insight 3: top selling category
+top_category = orig_df["Category"].value_counts().idxmax()
+insight3 = f"Top selling category: {top_category}"
+
+with open("insight1.txt", "w") as f:
+    f.write(insight1)
+
+with open("insight2.txt", "w") as f:
+    f.write(insight2)
+
+with open("insight3.txt", "w") as f:
+    f.write(insight3)
+
+print("3 insights saved")
+
+# Call visualize.py
+subprocess.run(["python", "visualize.py", sys.argv[1]])
